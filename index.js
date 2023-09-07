5a10d6a8a128310cf444a2c11f53649c9d5f806b
Trong Nghia <nghia.nt2704@gmail.com>
2023-09-07 15:31:16 +0700

    add static files

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const cors = require('cors');
 const port = process.env.PORT;
 const route = require('./routes');
 const db = require('./app/database/config');
+const path = require('path')
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
-app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(cors());
 
