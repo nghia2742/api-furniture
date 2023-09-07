f934fb0e1321874c40619f31bca5062cadbe006f
Trong Nghia <nghia.nt2704@gmail.com>
2023-09-07 14:26:42 +0700

    fixed db

--- app/database/config.js
+++ app/database/config.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const uri = process.env.DATABASE_URI;
 async function connect() {
     try {
-        await mongoose.connect(uri);
+        await mongoose.connect(uri,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            });
         console.log('Connect Successfully!!!');
     } catch (error) {
         console.log('Connect to Database failed!!!');
